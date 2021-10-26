9919ecd092a39b20ca0542ed5cfa4fc0b1f50bca
beiyuouo <beiyu233@gmail.com>
2021-10-26 17:57:28 +0800

    Create __init__.py

---
+++ fedhf/component/sampler/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File    :   fedhf\component\sampler\__init__.py
+@Time    :   2021-10-26 16:46:48
+@Author  :   Bingjie Yan
+@Email   :   bj.yan.pa@qq.com
+@License :   Apache License 2.0
+"""
